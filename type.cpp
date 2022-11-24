4aaf44256a48f26f67b67ab173b981606b7d8ade
Chmele <killerbbb123@gmail.com>
2022-11-25 00:46:12 +0200

    fix syntax errors(?)))

--- type.cpp
+++ type.cpp
@@ -1,6 +1,6 @@
-#include <string>;
-#include <vector>;
-#include <mutex>;
+#include <string>
+#include <vector>
+#include <mutex>
 
 using namespace std;
 
@@ -17,10 +17,10 @@ private:
 template <typename T>
 class TypedField : public Field {
 public:
-    TypedField(const std::string &name, const T &data) : Field(name), m_data(data);
+    TypedField(const std::string &name, const T &data) : Field(name), m_data(data) {}
 
 private:
     T m_data;
 };
 
-typedef vector<shared_ptr<Field>> property_list_type;
+typedef vector<shared_ptr<Field> > property_list_type;
