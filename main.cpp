bad7f6d42493c79c7a6438ef68e864d0dae1fe8d
Artyom Fisunenko <artyom.fisunenko@gmail.com>
2022-11-24 18:58:06 +0200

    coded some cpp with Ger

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "type.cpp"
+
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
---
+++ type.cpp
@@ -0,0 +1,26 @@
+#include <string>;
+#include <vector>;
+#include <mutex>;
+
+using namespace std;
+
+class Field
+{
+public:
+    Field(const string &name) : m_name(name) {}
+    virtual ~Field() {}
+
+private:
+    string m_name;
+};
+
+template <typename T>
+class TypedField : public Field {
+public:
+    TypedField(const std::string &name, const T &data) : Field(name), m_data(data);
+
+private:
+    T m_data;
+};
+
+typedef vector<shared_ptr<Field>> property_list_type;
